94f90812ce41b5958258ddbf42e7a2ba1291088e
jasons_xurshid <abdxurshid01@mail.ru>
2023-01-26 07:54:14 +0500

    2

--- Collections_lists/Collections_lists/LeetCode_Lists/Program.cs
+++ Collections_lists/Collections_lists/LeetCode_Lists/Program.cs
@@ -14,7 +14,45 @@ namespace Program
         }
     }
 
+    public class MyQueue
+    {
 
+        public MyQueue()
+        {
+
+        }
+        Stack stack1 = new();
+        Stack stack2 = new();
+        bool s1Enabled = true;
+        public void Push(int x)
+        {
+            if (s1Enabled) stack1.Push(x);
+           else stack2.Push(x);
+
+        }
+
+        public int Pop()
+        {
+            int counter = s1Enabled ? stack1.Count : stack2.Count;
+            for (int i = 0; i < counter; i++)
+            {
+                if (s1Enabled) stack2.Push(stack2.Pop());
+                else stack1.Push(stack2.Pop());
+            } s1Enabled = false;
+            return s1Enabled? (int)stack1.Pop() : (int)stack2.Pop();
+           
+        }
+
+        public int Peek()
+        {
+            return s1Enabled ? (int)stack1.Peek() : (int)stack2.Peek();
+        }
+
+        public bool Empty()
+        {
+            return stack1.Count == 0 & stack2.Count == 0 ? true : false;
+        }
+    }
 
     public class MyStack
     {
---
+++ Collections_lists/Collections_lists/MyQueue.cs
@@ -0,0 +1,7 @@
+﻿using System;
+namespace Collections_lists
+{
+	
+   
+}
+
