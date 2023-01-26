2fdc07ba5e8f7ffa7e0feaa8be9877a18aa1f6a9
jasons_xurshid <abdxurshid01@mail.ru>
2023-01-26 08:15:27 +0500

    asd

--- Collections_lists/Collections_lists/MyQueue.cs
+++ Collections_lists/Collections_lists/MyQueue.cs
@@ -1,7 +1,51 @@
 ﻿using System;
 namespace Collections_lists
 {
-	
-   
+
+    public class MyQueue
+    {
+
+        public MyQueue()
+        {
+
+        }
+        Stack stack1 = new();
+        Stack stack2 = new();
+        bool s1Enabled = true;
+        public void Push(int x)
+        {
+
+            for (int i = 0; i < stack2.Count; i++)
+            {
+                stack1.Push(stack2.Pop());
+            }
+            stack1.Push(x);
+        }
+
+        public int Pop()
+        {
+
+            for (int i = 0; i < stack1.Count; i++)
+            {
+                stack2.Push(stack1.Pop());
+            }
+            return (int)stack2.Pop();
+
+        }
+
+        public int Peek()
+        {
+            for (int i = 0; i < stack1.Count; i++)
+            {
+                stack2.Push(stack1.Pop());
+            }
+            return (int)stack2.Peek();
+        }
+
+        public bool Empty()
+        {
+            return stack1.Count == 0 & stack2.Count == 0 ? true : false;
+        }
+    }
 }
 
