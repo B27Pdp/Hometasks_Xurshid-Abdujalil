29813b69fc4fe441c97ff98c7b75832b3f783694
jasons_xurshid <abdxurshid01@mail.ru>
2023-01-27 10:36:08 +0500

    New file

---
+++ New_/L7_Collections2/Leetcode_Design-Hashset-705.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace L7_Collections2
+{
+	public class Leetcode_Design_Hashset_705
+	{
+		public Leetcode_Design_Hashset_705()
+		{
+		}
+        List<int> list = new();
+            public void Add(int key)
+            {
+               if(!list.Contains(key))
+                list.Add(key);
+            }
+
+            public void Remove(int key)
+            {
+               if (list.Contains(key))
+                list.Remove(key);
+            }
+
+            public bool Contains(int key)
+            {
+            return list.Contains(key);
+            }
+        
+
+    }
+}
+
