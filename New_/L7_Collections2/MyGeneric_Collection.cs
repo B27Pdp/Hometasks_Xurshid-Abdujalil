ccb6a8dd87b84600037f2da5b5f59c559e0b429b
jasons_xurshid <abdxurshid01@mail.ru>
2023-01-27 15:57:46 +0500

    adf

---
+++ New_/L7_Collections2/MyGeneric_Collection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace L7_Collections2
+{
+	public class MyGeneric_Collection<T,K,U> where T :unmanaged
+											 where K: new()
+											 where U: class
+	{
+		public MyGeneric_Collection()
+		{
+		}
+
+
+	}
+
+
+	public class StudentDetails
+	{
+		public string name;
+		public int age;
+		public StudentDetails(string name = "DefaultName")
+		{
+			this.name = name;
+		}
+		public static void Print_StudentDetails(StudentDetails student)
+		{
+			Console.WriteLine($"name :{student.name} , age :{student.age} ");
+		}
+	}
+	public interface IMyID
+	{
+		public StudentDetails ID  { get; set; }
+	}
+	public struct Group
+	{
+
+	}
+}
+
